5b5c39a745724429927cfa6739c4f22450fe4827
lucasdellamaggiore0 <dellamaggiorelucas25@gmail.com>
2022-09-04 11:50:00 -0300

    Agregado de comentarios, mejora de estilos

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const d = document
 
-//! Llamado con fetch para traer todos los productos
+//! Petición con fetch para traer todos los productos
 async function getProducts() {
     let data = await fetch('https://bsale-apib.herokuapp.com/')
     let dataJSON = await data.json()
@@ -8,7 +8,7 @@ async function getProducts() {
     return dataArray
 }
 
-//! Llamado con fectch para traer todas las categorías
+//! Petición con fectch para traer todas las categorías
 
 async function getCategories() {
     await fetch('https://bsale-apib.herokuapp.com/categories')
@@ -16,15 +16,17 @@ async function getCategories() {
         .then(data => renderSelectCategories(data.categories))
 }
 
+
+//! Petición con fectch para traer el detalle de cada producto a traves de su id
 async function getProductsByCategory(id) {
     let data = await fetch(`https://bsale-apib.herokuapp.com/?category=${id}`)
     let dataJSON = await data.json()
     let dataArray = await dataJSON.products
-    // return dataArray
     d.getElementById('cards-container').innerHTML = ""
     renderProducts(dataArray)
 }
 
+//! Petición con fectch para la búsqueda de productos a traves de su nombre
 async function searchProducts(name) {
     let data = await fetch(`https://bsale-apib.herokuapp.com/?name=${name}`)
     let dataJSON = await data.json()
@@ -32,7 +34,7 @@ async function searchProducts(name) {
     return dataArray
 }
 
-
+//! Función encargada del renderizado de categorias en el select
 async function renderSelectCategories(categories) {
     const selectCategories = d.getElementById('categories')
     const allProducts = await getProducts()
@@ -53,6 +55,7 @@ async function renderSelectCategories(categories) {
     })
 }
 
+//! Función encargada de renderizar los productos a través de la barra de búsqueda de manera dinámica
 function renderSearchProducts() {
     d.getElementById('input-search').addEventListener('keyup', async (e) => {
         let products = await searchProducts(e.target.value)
@@ -61,45 +64,38 @@ function renderSearchProducts() {
     })
 }
 
+//! Función encargada de renderizar los productos
 function renderProducts(products) {
-    for (let i = 0; i < products.length; i++) {
-        const productName = d.createElement('h3')
-        productName.textContent = products[i].name
+        for (let i = 0; i < products.length; i++) {
+            //! Creación de nodos para luego insertarlos en el DOM
+            const productName = d.createElement('h3')
+            productName.textContent = products[i].name
 
-        const productImg = d.createElement('img')
-        productImg.src = products[i].url_image
+            const productImg = d.createElement('img')
+            productImg.src = products[i].url_image
 
-        const productPrice = d.createElement('p')
-        productPrice.textContent = "$" + products[i].price
+            const productPrice = d.createElement('p')
+            productPrice.textContent = "$" + products[i].price
 
-        const divContainer = d.getElementById('cards-container')
+            const divContainer = d.getElementById('cards-container')
 
-
-        const cardContainer = d.createElement('div')
-        cardContainer.className = 'product-card'
-        cardContainer.appendChild(productName)
-        cardContainer.appendChild(productImg)
-        cardContainer.appendChild(productPrice)
-        divContainer.appendChild(cardContainer)
-    }
-
+            const cardContainer = d.createElement('div')
+            
+            //! Agregado de nodos a sus respectivos padres
+            cardContainer.className = 'product-card'
+            cardContainer.appendChild(productName)
+            cardContainer.appendChild(productImg)
+            cardContainer.appendChild(productPrice)
+            divContainer.appendChild(cardContainer)
+        }
 }
 
-//! Renderizado de productos en el front
 
+//! Función inicializadora
 async function init() {
     let allProducts = await getProducts()
     renderProducts(allProducts)
     await getCategories()
-    // const inputSearch = d.getElementById('input-search')
-    // inputSearch.addEventListener('change',async (e) => {
-    //     e.preventDefault()
-    //     let inputContent = e.target.value
-    //     let data = await searchProducts(inputContent)
-    //     d.getElementById('cards-container').innerHTML = ""
-    //     renderProducts(data)
-    //     // console.log(inputContent)
-    // })
     renderSearchProducts()
 }
 
